7fd2dbdd20c5ab6a5634c5b11645dca372649134
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-05-30 15:00:52 +0900

    v3

--- cs0530/Form1.cs
+++ cs0530/Form1.cs
@@ -14,6 +14,8 @@ namespace cs0530
             label1.Top += vy;
             if (label1.Left < 0)
                 vx = -vx;
+            if (label1.Top < 0)
+                vy = -vy;
         }
     }
 }
