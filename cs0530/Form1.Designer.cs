3849b47d786d97b2f45f907fcfa68f3cfa0b3356
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-06-13 15:09:21 +0900

    random

--- cs0530/Form1.Designer.cs
+++ cs0530/Form1.Designer.cs
@@ -31,28 +31,66 @@
             components = new System.ComponentModel.Container();
             label1 = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(169, 319);
+            label1.Font = new Font("Yu Gothic UI", 20F);
+            label1.Location = new Point(125, 182);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(90, 37);
             label1.TabIndex = 0;
             label1.Text = "label1";
             // 
             // timer1
             // 
             timer1.Enabled = true;
-            timer1.Interval = 1;
             timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 25F);
+            label2.Location = new Point(271, 151);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 46);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI", 20F);
+            label3.Location = new Point(105, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 37);
+            label3.TabIndex = 2;
+            label3.Text = "0";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(385, 248);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 57);
+            button1.TabIndex = 3;
+            button1.Text = "リトライ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Visible = false;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(493, 306);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -64,5 +102,8 @@
 
         private Label label1;
         private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
     }
 }
--- cs0530/Form1.cs
+++ cs0530/Form1.cs
@@ -3,20 +3,34 @@ namespace cs0530
     public partial class Form1 : Form
     {
         int vx = -20, vy = -20;
+        int counter = 0;
+        static Random random = new Random();
         public Form1()
         {
             InitializeComponent();
+            vx = random.Next(-10, 11);
+            vy = random.Next(-10, 11);
+
+            label1.Left = random.Next(0,ClientSize.Width-label1.Width);
+            label1.Top = random.Next(0, ClientSize.Height - label1.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            var mpos = MousePosition;
+            var fpos = PointToClient(mpos);
+            Text = $"{mpos.X},{mpos.Y}/{fpos.X},{fpos.Y}";
+
+            label2.Left = fpos.X - (label2.Width / 2);
+            label2.Top = fpos.Y - (label2.Height / 2);
+
             label1.Left += vx;
             label1.Top += vy;
             if (label1.Left < 0)
-            { 
+            {
                 vx = Math.Abs(vx);
             }
-            else if(label1.Right>ClientSize.Width)
+            else if (label1.Right > ClientSize.Width)
             {
                 vx = -Math.Abs(vx);
             }
@@ -24,10 +38,45 @@ namespace cs0530
             {
                 vy = Math.Abs(vy);
             }
-            else if(label1.Bottom> ClientSize.Height)
+            else if (label1.Bottom > ClientSize.Height)
             {
                 vy = -Math.Abs(vy);
             }
+
+            counter++;
+            label3.Text = $"{counter}";
+
+            if (
+                (label1.Left < fpos.X)
+                &&
+                (label1.Right > fpos.X)
+                &&
+                (label1.Top < fpos.Y)
+                &&
+                (label1.Bottom > fpos.Y)
+                )
+            {
+                timer1.Enabled = false;
+                button1.Visible = true;
+            }
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            counter = 0;
+            button1.Visible= false;
+            vx = random.Next(-10, 11);
+            vy = random.Next(-10, 11);
+
+            label1.Left = random.Next(ClientSize.Width - label1.Width);
+            label1.Top = random.Next(ClientSize.Height - label1.Height);
         }
     }
 }
